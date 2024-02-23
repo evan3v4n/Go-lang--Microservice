726302ed3b5a8898092327f9fd31957548219db1
evan3v4n <evan.scott2050@Gmail.com>
2024-02-23 13:57:05 -0500

    crud handlers

--- application/routes.go
+++ application/routes.go
@@ -7,9 +7,10 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/evan3v4n/Go-lang--Microservice/handler"
+	"github.com/evan3v4n/Go-lang--Microservice/repository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -18,17 +19,21 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetByID)
 	router.Put("/{id}", orderHandler.UpdateByID)
-	router.Delete("/", orderHandler.DeleteByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
